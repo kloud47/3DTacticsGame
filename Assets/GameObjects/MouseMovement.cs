b22a00a007fc442bc61be90438325ddae862d229
kloud47 <136368351+kloud47@users.noreply.github.com>
2025-08-12 14:00:11 +0530

    Initialized the Game Project

---
+++ Assets/GameObjects/MouseMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    private static MouseMovement instance; // Making a permanent instance in the world:
+    
+    [SerializeField] private LayerMask tilePlaneLayerMask;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        transform.position = MouseMovement.GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, float.MaxValue, instance.tilePlaneLayerMask);
+        return hit.point;
+    }
+}
